844274ce6b943737512eb352afa0531cfffc5b47
zaleim <43824588+zaleim@users.noreply.github.com>
2018-11-05 00:40:03 +0200

    Add files via upload

---
+++ landingpage_01/js/script.js
@@ -0,0 +1,56 @@
+  $(document).ready(function(){
+         // for default-fixed menu
+        var $menu = $("#menu");
+            
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 100 && $menu.hasClass("default")){
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("default")
+                           .addClass("fixed transbg")
+                           .fadeIn('fast');
+                });
+            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed")) {
+                $menu.fadeOut('fast',function(){
+                    $(this).removeClass("fixed transbg")
+                           .addClass("default")
+                           .fadeIn('fast');
+                });
+            }
+        });   
+
+
+        // for search
+        focusMethod = function getFocus() {           
+      document.getElementById("myTextField").focus();
+    }
+
+
+// for loginRegistr form
+
+const buttons = Array.from(document.querySelectorAll('.buttons > button'));
+        const activeElements = Array.from(document.querySelectorAll('[data-action="animated"]'));
+
+        function toggle() {
+          activeElements.forEach(item => {
+            item.classList.toggle('signup-button-active');
+            item.classList.toggle('login-button-active');
+          });
+        };
+
+        buttons.forEach(item => item.addEventListener('click', toggle));
+
+
+// for first scrin
+$('.header').height($(window).height());
+
+// for link-menu
+
+ $(".navbar a").click(function(){
+  $("body,html").animate({
+   scrollTop:$("#" + $(this).data('value')).offset().top
+  },1000)
+  
+ })
+
+
+ });
